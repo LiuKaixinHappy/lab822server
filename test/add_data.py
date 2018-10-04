e006590cdcc31ae17a4bbdd84bd0ed2ffaaed415
LiuKaixin <644306737@qq.com>
2018-10-04 16:28:04 +0800

    update db data

--- test/add_data.py
+++ test/add_data.py
@@ -14,6 +14,7 @@ class ImgParam(Document):
     name = StringField(required=True)
     value = ListField()
     limit = StringField()
+    pName = StringField()
 
 
 class ImgOperation(Document):
@@ -28,31 +29,37 @@ def insert():
     type2 = ImgType(name='边缘检测').save()
     type3 = ImgType(name='轮廓提取').save()
 
-    param1 = ImgParam(type='input', name='卷积核宽', value=[], limit='odd').save()
-    param2 = ImgParam(type='input', name='卷积核高', value=[], limit='odd').save()
-    param3 = ImgParam(type='input', name='水平方向标准差', value=[], limit='').save()
-    param4 = ImgParam(type='input', name='垂直方向标准差', value=[], limit='').save()
-    param5 = ImgParam(type='choice', name='边界扩充方式',
+    param1 = ImgParam(type='input', name='卷积核宽', value=[], limit='odd', pName='kSizeW').save()
+    param2 = ImgParam(type='input', name='卷积核高', value=[], limit='odd', pName='kSizeH').save()
+    param3 = ImgParam(type='input', name='水平方向标准差', value=[], limit='', pName='sigmaX').save()
+    param4 = ImgParam(type='input', name='垂直方向标准差', value=[], limit='', pName='sigmaY').save()
+    param5 = ImgParam(type='select', name='边界扩充方式',
                       value=['边界复制', '常数扩充', '反射扩充', '边界为中心反射扩充', '平铺扩充'],
-                      limit='').save()
-    param6 = ImgParam(type='input', name='算子宽', value=[], limit='').save()
-    param7 = ImgParam(type='input', name='算子高', value=[], limit='').save()
-    param8 = ImgParam(type='input', name='锚点', value=[], limit='').save()
-    param9 = ImgParam(type='input', name='窗口大小', value=[], limit='>1').save()
-    param10 = ImgParam(type='input', name='权重模板宽', value=[], limit='').save()
-    param11 = ImgParam(type='input', name='sigma color', value=[], limit='').save()
-    param12 = ImgParam(type='input', name='sigma space', value=[], limit='').save()
-    param13 = ImgParam(type='input', name='权重模板高', value=[], limit='').save()
-    param14 = ImgParam(type='input', name='空间距离权重模板标准差', value=[], limit='').save()
-    param15 = ImgParam(type='input', name='相似性权重模板标准差', value=[], limit='').save()
+                      limit='', pName='borderType').save()
+    param6 = ImgParam(type='input', name='算子宽', value=[], limit='', pName='kSizeW').save()
+    param7 = ImgParam(type='input', name='算子高', value=[], limit='', pName='kSizeH').save()
+    param9 = ImgParam(type='input', name='窗口大小', value=[], limit='int', pName='kSize').save()
+    param10 = ImgParam(type='input', name='滤波像素邻域直径', value=[], limit='', pName='d').save()
+    param11 = ImgParam(type='input', name='sigma color', value=[], limit='', pName='sigmaColor').save()
+    param12 = ImgParam(type='input', name='sigma space', value=[], limit='', pName='sigmaSpace').save()
+    param8 = ImgParam(type='input', name='权重模板宽', value=[], limit='', pName='kSizeW').save()
+    param13 = ImgParam(type='input', name='权重模板高', value=[], limit='', pName='kSizeH').save()
+    param14 = ImgParam(type='input', name='空间距离权重模板标准差', value=[], limit='', pName='sigmaG').save()
+    param15 = ImgParam(type='input', name='相似性权重模板标准差', value=[], limit='', pName='sigmaD').save()
 
     operation1 = ImgOperation(name='高斯平滑', code='101', type=[type1],
                               params=[param1, param2, param3, param4, param5]).save()
     operation2 = ImgOperation(name='均值平滑', code='102', type=[type1],
-                              params=[param6, param7, param8, param5]).save()
+                              params=[param6, param7, param5]).save()
+    operation3 = ImgOperation(name='中值平滑', code='103', type=[type1],
+                              params=[param9]).save()
+    operation4 = ImgOperation(name='双边滤波', code='104', type=[type1],
+                              params=[param10, param11, param12, param5]).save()
+    operation5 = ImgOperation(name='联合双边滤波', code='105', type=[type1],
+                              params=[param8, param13, param14, param15, param5]).save()
 
 
-# insert()
+insert()
 
 o1s = list(ImgOperation.objects(type__name='图像平滑').exclude('id', 'name', 'code'))
 
